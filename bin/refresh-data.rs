32df2628e10f06565990305e685443b8ca897859
Venus Xeon-Blonde <venus@vcfxb.org>
2026-04-20 04:02:11 -0400

    scryfall bulk pull script

--- bin/refresh-data.rs
+++ bin/refresh-data.rs
@@ -1,1 +1,61 @@
-fn main() {}
+//! Refresh the local copy of scryfall's bulk data export.
+
+use std::path::PathBuf;
+use std::sync::LazyLock;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use log::LevelFilter;
+use tokio::fs::File;
+use tokio::io::{AsyncWriteExt, BufWriter};
+use url::Url;
+use moxfield_cli::scryfall::client::ScryfallClient;
+use futures::StreamExt;
+
+static MULTIPROGRESS: LazyLock<MultiProgress> = LazyLock::new(|| MultiProgress::new());
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let logger = env_logger::Builder::from_default_env().filter_level(LevelFilter::Info).build();
+    let bridge = indicatif_log_bridge::LogWrapper::new(MULTIPROGRESS.clone(), logger);
+    bridge.try_init().unwrap();
+
+    let scryfall_client = ScryfallClient::new();
+    let bulk_data = scryfall_client.bulk_data().await?;
+    let all_cards = bulk_data["data"]
+        .as_array()
+        .expect("data is array")
+        .iter()
+        .filter_map(|v| v.as_object())
+        .find(|v| v["type"] == "all_cards")
+        .expect("found all_cards");
+
+    let uri = all_cards["download_uri"].as_str().unwrap().parse::<Url>()?;
+    let file_name = uri.path_segments().unwrap().rev().next().unwrap();
+    let size = all_cards["size"].as_i64().unwrap();
+
+    static DOWNLOAD_STYLE: LazyLock<ProgressStyle> = LazyLock::new(|| {
+        ProgressStyle::with_template("[{decimal_bytes} / {decimal_total_bytes} {elapsed}] {msg} {wide_bar} {decimal_bytes_per_sec}").unwrap()
+    });
+
+    let progress = MULTIPROGRESS.add(ProgressBar::new(size as u64));
+    progress.set_style(DOWNLOAD_STYLE.clone());
+    progress.set_message("pulling bulk data");
+
+    let response = reqwest::get(uri.clone()).await?;
+    let mut byte_stream = response.bytes_stream();
+
+    if !PathBuf::from("scryfall_pull").exists() {
+        tokio::fs::create_dir("scryfall_pull").await?;
+    }
+
+    let file = File::create(format!("scryfall_pull/{file_name}")).await?;
+    let mut writer = BufWriter::new(file);
+
+    while let Some(next) = byte_stream.next().await {
+        let bytes_written = writer.write(next?.as_ref()).await?;
+        progress.inc(bytes_written as u64);
+    }
+
+    progress.finish();
+
+    Ok(())
+}
---
+++ src/scryfall/client.rs
@@ -0,0 +1,64 @@
+use governor::{DefaultDirectRateLimiter, Quota, RateLimiter};
+use reqwest::{Client, Method};
+use reqwest::header::HeaderMap;
+use std::num::NonZeroU32;
+use std::sync::Arc;
+use governor::clock::{QuantaClock, Clock, DefaultClock};
+use serde::de::DeserializeOwned;
+use serde_json::Value;
+use thiserror::Error;
+use tokio::io::AsyncWrite;
+
+pub struct ScryfallClient {
+    client: Client,
+    rl: Arc<DefaultDirectRateLimiter>,
+}
+
+#[derive(Error, Debug)]
+pub enum ScryfallClientError {
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+}
+
+impl ScryfallClient {
+    const ROOT_URL: &'static str = "https://api.scryfall.com";
+
+    pub fn new() -> Self {
+        let mut headers = HeaderMap::new();
+
+        headers.insert(
+            "User-Agent",
+            format!("moxfield-cli/{}", env!("CARGO_PKG_VERSION"))
+                .parse()
+                .unwrap(),
+        );
+        headers.insert("Accept", "application/json".parse().unwrap());
+
+        let client = Client::builder().default_headers(headers).build().unwrap();
+
+        ScryfallClient {
+            client,
+            rl: Arc::new(RateLimiter::direct(Quota::per_second(
+                const { NonZeroU32::new(10).unwrap() },
+            ))),
+        }
+    }
+
+    async fn call<T: DeserializeOwned>(&self, method: Method, route: impl AsRef<str>) -> reqwest::Result<T> {
+        if let Err(not_until) = self.rl.check() {
+            let now = DefaultClock::default().now();
+            log::warn!("Hit Scryfall API ratelimit, waiting {:?}", not_until.wait_time_from(now));
+            self.rl.until_ready().await;
+        }
+
+        self.client.request(method, format!("{}/{}", Self::ROOT_URL, route.as_ref()))
+            .send()
+            .await?
+            .json()
+            .await
+    }
+
+    pub async fn bulk_data(&self) -> reqwest::Result<Value> {
+        self.call(Method::GET, "bulk-data").await
+    }
+}
